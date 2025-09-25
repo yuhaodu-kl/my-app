54773a254ffbc87a80b155d959598496d3d03d70
yuhadou <yuhaoduxian@gmail.com>
2025-09-24 22:29:32 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from VS Code 👋</h1>
+      <p>I’m coding my first React app!</p>
+    </div>
+  );
+}
+
+export default App;
